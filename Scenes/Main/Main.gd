301d9f85131f81c382b6cb1eefa58679f9e6703f
plexsoup <plexsoup@gmail.com>
2019-04-26 16:13:05 -0700

    Moved all folders to object-centric hierarchy instead of filetype-centric
    and renamed files and folders to PascalCase

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -3,9 +3,9 @@ extends Node
 var current_level
 
 func _ready():
-	
+
 	var load_level = preload("res://scenes/template_level.tscn")
-	
+
 	current_level = load_level.instance()
 	self.add_child(current_level)
 
@@ -14,11 +14,11 @@ func _ready():
 #	pass
 
 func _on_Signal_level_changed():
-	
+
 	var load_level = preload("res://scenes/template_level.tscn")
-	
+
 	self.remove_child(current_level)
-	
+
 	current_level = load_level.instance()
 	add_child(current_level)
 
