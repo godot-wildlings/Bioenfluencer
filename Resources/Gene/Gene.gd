3b1537a90ce23db931182784b90de2e2a0a7645d
Kevin von Spiczak <caevv@me.com>
2019-04-28 01:31:56 +0200

    remove CATEGORY_BODY form Gene.gd,add crafting btn

--- Resources/Gene/Gene.gd
+++ Resources/Gene/Gene.gd
@@ -2,11 +2,11 @@ extends Resource
 
 class_name Gene, "res://Resources/Gene/gene_icon.png"
 
-enum GeneCategory { CATEGORY_BODY, CATEGORY_MOVEMENT, CATEGORY_COLOR }
+enum GeneCategory { CATEGORY_MOVEMENT, CATEGORY_COLOR }
 
 export var name : String = "Gene" setget _set_name
 #warning-ignore:unused_class_variable
-export(GeneCategory) var category : int = GeneCategory.CATEGORY_BODY
+export(GeneCategory) var category : int = GeneCategory.CATEGORY_COLOR
 #warning-ignore:unused_class_variable
 export var number_of_followers : int = 0
 #warning-ignore:unused_class_variable
