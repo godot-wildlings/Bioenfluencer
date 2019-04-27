972111ce02d11db0e9ef6f8e33931a5100e80651
Kevin von Spiczak <caevv@me.com>
2019-04-27 21:50:38 +0200

    add bodypart state

--- Scenes/BodyPart/BodyPart.gd
+++ Scenes/BodyPart/BodyPart.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-enum BodyPartCategory { HEAD, TORSO, LEGS, TAIL }
+enum BodyPartCategory { HEAD, TORSO, LEGS, TAIL } # Arms?
 
 var genes : Array
 
@@ -10,6 +10,12 @@ export(BodyPartCategory) var category : int = BodyPartCategory.HEAD
 #warning-ignore:unused_class_variable
 export var icon : Texture = preload("res://icon.png") as Texture
 
+enum States { GUI, CREATURE }
+var state : int = States.GUI
 
 func _ready() -> void:
+	if state == States.GUI:
+		$Sprite.scale = Vector2(0.25, 0.25)
+	elif state == States.CREATURE:
+		$Sprite.scale = Vector2(1, 1)
 	data_store.body_parts[part_name] = self
