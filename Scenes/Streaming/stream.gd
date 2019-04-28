84135f3a1b4b16b0ccfa57b341fb4a20cdac416d
DoggieBalloon <armaggeddonchief@gmail.com>
2019-04-29 06:45:09 +0800

    wip-displaygraphics

--- Scenes/Streaming/stream.gd
+++ Scenes/Streaming/stream.gd
@@ -4,6 +4,7 @@ extends Node
 #is just a draft
 
 onready var creatures_display = $stream_UI/RightSide/ItemList
+onready var display_position = $stream_UI/Control/Position2D
 
 var trends
 
@@ -12,10 +13,23 @@ func _ready():
 	for i in DataStore.crafted_creatures:
 
 		if i is Creature and is_instance_valid(i):
-			creatures_display.add_item(i.creature_name,null,true)
+			
+			creatures_display.add_item(i.creature_name,null, true)
+#			creatures_display.add_item(i.creature_name,DataStore.crafted_creatures.texture, true)
 
 #func _process(delta):
-#	pass
+#
+#	if creatures_display.is_anything_selected():
+#
+#		for i in creatures_display.get_selected_items():
+#			for j in DataStore.crafted_creatures:
+#				if j.is_selected():
+#
+#					var creature_selected = i.instance()
+#
+#					display_position.add_child(creature_selected)
+	
+	pass
 
 func _on_Button2_pressed():
 
