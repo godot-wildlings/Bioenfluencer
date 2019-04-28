8e9c2d5c2a527ba11d9d0a66710c2c49547242a1
plexsoup <plexsoup@gmail.com>
2019-04-27 17:00:01 -0700

    tweaked main ui a bit

--- Scenes/Main/HSlider.gd
+++ Scenes/Main/HSlider.gd
@@ -8,12 +8,16 @@ extends HSlider
 func _ready():
 	min_value = 0.0001
 	step = 0.0001
+	set_volume(value)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func set_volume(ratio : float):
+	ratio = clamp(ratio, 0, 1.0)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), log(ratio) * 20)
 
 func _on_HSlider_value_changed(value):
-	
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), log(value) * 20)
+
+	set_volume(value)
